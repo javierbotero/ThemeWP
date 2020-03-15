e4cc890927ff755a2f596e3feb51f0100908fe38
Javier Botero <javierbotero10271@gmail.com>
2020-03-15 13:37:47 -0500

    Add header and footer, content needs to be added

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php bloginfo("name"); ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <header class="site-header">
+      <h1><a href="<?php echo home_url(); ?>"><?php bloginfo("name"); ?></a></h1>
+      <h3><?php bloginfo("description"); ?></h3>
+    </header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+get_header();
+
+  $counter = 0;
+  while (have_posts()) {
+      the_post(); ?>
+
+      <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+      <?php get_header() ?>
+      <p><?php the_content() ?></p>
+
+  <?php }
+
+get_footer();
+
+?>
