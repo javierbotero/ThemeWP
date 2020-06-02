cfb07547fdfb1be6efe871463896da13e824e09b
Javier Botero <javierbotero10271@gmail.com>
2020-06-01 23:14:36 -0500

    Add child pages after title

--- page.php
+++ page.php
@@ -5,6 +5,13 @@ get_header();
   while (have_posts()) {
       the_post(); ?>
       <h1>This is the template for all the pages</h1>
+      <?php
+        $array = array (
+          'child_of' => $post -> ID,
+          'title_li' => ''
+        );
+      ?>
+      <?php wp_list_pages($array); ?>
       <article class="post page">
         <h1><?php the_title() ?></h1>
         <p><?php the_content() ?></p>
