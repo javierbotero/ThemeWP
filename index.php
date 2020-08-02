f5fcd3ff546b4eb16aa521e9e2e1faa56b626b40
Javier Botero <javierbotero10271@gmail.com>
2020-08-01 23:43:30 -0500

    Add posts meta data, author and categories, with proper links

--- index.php
+++ index.php
@@ -6,6 +6,19 @@ get_header();
       the_post(); ?>
       <article class="post">
         <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+        <p><?php the_time("F jS, Y g:i a"); ?> |
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> |
+            <?php
+                $categories = get_the_category();
+                $cats = '';
+                foreach ($categories as $category) {
+                    $cats .= "<a href='" . get_category_link($category->term_id) . 
+                    "'>" . $category->cat_name . "</a>" . ", ";
+                }
+
+                echo trim($cats, ', ');
+            ?>
+        </p>
         <p><?php the_content() ?></p>
       </article>
   <?php }
